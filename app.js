e2c59d9fbe638d234dcff97df7e7783e00dcf587
sinke100 <sinke1985@gmail.com>
2023-06-21 19:01:33 +0200

    sv parcijalni

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = express();
 const path = require('path');
 const router = express.Router();
 
-router.get('/', function(req, res) {
+router.get('/', function (req, res) {
   res.sendFile(path.join(__dirname + '/index.html'));
 });
 
