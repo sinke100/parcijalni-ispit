bd537938b804ea728e7dfd62869bde736415a7cb
sinke100 <sinke1985@gmail.com>
2023-06-21 20:06:12 +0200

    sv parcijalni dorada fileova

--- app.js
+++ app.js
@@ -1,13 +1,25 @@
-const express = require('express');
+import random from 'random';
+import express from 'express';
+import { fileURLToPath } from 'url';
+import path, { dirname } from 'path';
+
 const app = express();
-const path = require('path');
 const router = express.Router();
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 router.get('/', function (req, res) {
-  res.sendFile(path.join(__dirname + '/index.html'));
+  res.sendFile(path.join(__dirname, 'index.html'));
 });
 
-app.use('/assets', express.static(__dirname + '/assets'));
+router.get('/random', function (req, res) {
+  const randomFloat = random.float();
+  res.json({ randomFloat: randomFloat });
+});
+
+app.use('/assets', express.static(path.join(__dirname, 'assets')));
+app.use(express.static(__dirname, { "extensions": ["css"] }));
 app.use('/', router);
 
 app.listen(process.env.port || 3000);
